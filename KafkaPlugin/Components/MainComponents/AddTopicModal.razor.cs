5b4e90ec6f35551cbbb62c2f98fcfbabba5d2b31
Юрий Кузнецов <omega131995@outlook.com>
2024-12-22 16:05:14 +0700

    Доработка внешнего вида, еще всякие мелочи

--- KafkaPlugin/Components/MainComponents/AddTopicModal.razor.cs
+++ KafkaPlugin/Components/MainComponents/AddTopicModal.razor.cs
@@ -1,18 +1,13 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
-using System.Threading.Tasks;
-using Contracts.Components;
-using KafkaPlugin.Database;
-using KafkaPlugin.Database.Database;
+﻿using Contracts.Components;
 using Microsoft.AspNetCore.Components;
 
 namespace KafkaPlugin.Components.MainComponents;
 
-public partial class AddHostModal : ComponentBase
+public partial class AddTopicModal : ComponentBase
 {
     private Modal? _modalRef;
-    private AddHostModel _addHostModel = new();
-
+    private AddTopicModel _model = new();
+    
     private RenderFragment? _openModalButton;
     private RenderFragment? _closeModalButton;
 
@@ -37,34 +32,11 @@ public partial class AddHostModal : ComponentBase
             builder.CloseElement();
         };
     }
-    
-    private Task HandleClose()
-    {
-        Console.WriteLine("Модальное окно закрыто.");
-        return Task.CompletedTask;
-    }
 
-    private async Task OnSubmitHandlerAsync()
+    private class AddTopicModel
     {
-        await using var context = new Context();
-
-        context.Add(new Host()
-        {
-            Ip = _addHostModel.Ip,
-            Port = _addHostModel.Port,
-        });
-        
-        await context.SaveChangesAsync();
-        
-        _modalRef?.Hide();
-    }
-    
-    private class AddHostModel
-    {
-        [Required(ErrorMessage = "Заполните IP адрес")]
-        public string Ip { get; set; } = string.Empty;
-        
-        [Required(ErrorMessage = "Заполните порт")]
-        public int Port { get; set; } = 0;
+        public string Name { get; set; } = string.Empty;
+        public int Partition { get; set; } = 0;
+        public int ReplicationFactor { get; set; } = 0;
     }
 }
--- KafkaPlugin/Components/MainPage.razor.cs
+++ KafkaPlugin/Components/MainPage.razor.cs
@@ -1,21 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using KafkaPlugin.Database;
-using KafkaPlugin.Interfaces.Providers;
-using KafkaPlugin.Models.Repositories;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
 
 namespace KafkaPlugin.Components;
 
 public partial class MainPage : ComponentBase
 {
-    [Inject] private IKafkaRepositoryProvider _kafkaRepositoryProvider { get; set; }
 
-    private List<Broker> brokers;
-    
-    protected override async Task OnInitializedAsync()
-    {
-        brokers = await _kafkaRepositoryProvider.BrokerRepository.GetAllAsync();
-    }
 }
